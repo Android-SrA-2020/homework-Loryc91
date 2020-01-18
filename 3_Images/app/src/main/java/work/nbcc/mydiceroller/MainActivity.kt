77c49388bf2c9245e4adb452ab7047fae37963e8
Larissa Oliveira <lari.mello91@gmail.com>
2020-01-18 16:02:51 -0400

    codelabs

--- 3_Images/app/src/main/java/work/nbcc/mydiceroller/MainActivity.kt
+++ 3_Images/app/src/main/java/work/nbcc/mydiceroller/MainActivity.kt
@@ -6,16 +6,22 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import work.nbcc.mydiceroller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage : ImageView
-    lateinit var diceImage2 : ImageView
+    private lateinit var diceImage : ImageView
+    private lateinit var diceImage2 : ImageView
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
@@ -31,7 +37,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getRandomDiceImage() : Int {
-
         return when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
